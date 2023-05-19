ac3df5b5d1008349139fe611a511f5b45123ee5e
Migue-San <lx15726@innovaccion.mx>
2023-05-18 18:33:07 -0600

    quitamos la hoja de estilos utilizando style-components

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-
+import styled from "styled-components";
 import Title from "../Title";
 import Account from "../Account";
 
+const StyledContainer = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+
+const StyledContend = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`
+
 const Container = () => {
   return (
-    <div className="container">
+    <StyledContainer>
       <Title>Smart Bank</Title>
-      <section className="content">
+      <StyledContend>
         <Account />
-      </section>
-    </div>
+        <Account />
+      </StyledContend>
+    </StyledContainer>
   );
 };
 
