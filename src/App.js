e8d6a6a897052caca913b81fc9a7485ae9abb8ff
Migue-San <lx15726@innovaccion.mx>
2023-05-19 17:22:42 -0600

    PROYECTO TERMINADO CAMBIANDO TEMA CLARO A OSCURO

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Header from "./Components/Header";
 import Container from "./Components/Container";
 import GlobalStyle from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";//para el tema oscuro
+import { temaClaro, temaOscuro } from "./Components/UI/temas";
+import { BtnTema } from "./Components/UI";
+import SwichertTema from "./Components/SwichertTema";
 
 function App() {
+  const [tema, setTema] = useState(true)
+
+  const toggleTema = () => {
+    setTema(() => !tema)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={tema ? temaClaro : temaOscuro}>
       <GlobalStyle/>
+      <BtnTema onClick={toggleTema}>
+        <SwichertTema tema={tema}/>
+      </BtnTema>
       <Header />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import Title from "../Title";
 import Account from "../Account";
+import List from "../List";
 
 const StyledContainer = styled.div`
-  background-color: #f1f1f1;
+  background-color: ${({theme}) => theme.body};
   min-height: 90vh;
   padding: 0px 15vw;
 `
@@ -24,7 +25,7 @@ const Container = () => {
       <Title>Smart Bank</Title>
       <StyledContend>
         <Account />
-        <Account />
+        <List/>
       </StyledContend>
     </StyledContainer>
   );
